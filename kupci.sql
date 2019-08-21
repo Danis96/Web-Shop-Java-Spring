77d17399798d7d4cc3c36516c681be8d5bab98b8
Danis96 <49531974+Danis96@users.noreply.github.com>
2019-08-21 13:53:10 +0200

    Add files via upload

---
+++ kupci.sql
@@ -0,0 +1,5 @@
+CREATE TABLE  `kupci` (
+`kupci_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`kupci_ime` varchar(200) NOT NULL,
+`korpa` text
+);
---
+++ proizvodi.sql
@@ -0,0 +1,4 @@
+CREATE TABLE  `proizvodi` (
+`proizvod_id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+`prozvod_ime` varchar(200) NOT NULLkupci
+);
